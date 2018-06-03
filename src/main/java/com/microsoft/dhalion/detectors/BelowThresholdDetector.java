b9b76b798ba9639c944ecbf7fd44dd503419747f
avflor <avflor@microsoft.com>
2018-06-02 17:46:59 -0700

    Adapted threshold detectors to use history

--- src/main/java/com/microsoft/dhalion/detectors/BelowThresholdDetector.java
+++ src/main/java/com/microsoft/dhalion/detectors/BelowThresholdDetector.java
@@ -4,6 +4,7 @@ import com.microsoft.dhalion.Utils;
 import com.microsoft.dhalion.conf.PolicyConfig;
 import com.microsoft.dhalion.core.Measurement;
 import com.microsoft.dhalion.core.MeasurementsTable;
+import com.microsoft.dhalion.core.MeasurementsTable.SortKey;
 import com.microsoft.dhalion.core.Symptom;
 
 import javax.inject.Inject;
@@ -15,9 +16,11 @@ public class BelowThresholdDetector extends BaseDetector {
   public static String SYMPTOM_LOW = BelowThresholdDetector.class.getSimpleName();
 
   public static final String LOW_THRESHOLD_CONF = "BelowThresholdDetector.threshold";
+  public static final String BELOW_THRESHOLD_NO_CHECKPOINTS = "BelowThresholdDetector.noCheckpoints";
 
   private final double lowThreshold;
   private String metricName;
+  private double noCheckpoints;
 
   private static final Logger LOG = Logger.getLogger(BelowThresholdDetector.class.getSimpleName());
 
@@ -25,28 +28,32 @@ public class BelowThresholdDetector extends BaseDetector {
   public BelowThresholdDetector(PolicyConfig policyConfig, String metricName) {
     this.lowThreshold = (Double) policyConfig.getConfig(Utils.getCompositeName(LOW_THRESHOLD_CONF, metricName));
     this.metricName = metricName;
+    this.noCheckpoints = (Double) policyConfig.getConfig(Utils.getCompositeName(BELOW_THRESHOLD_NO_CHECKPOINTS,
+                                                                                metricName), 1);
   }
 
   @Override
   public Collection<Symptom> detect(Collection<Measurement> measurements) {
     ArrayList<Symptom> result = new ArrayList<>();
-    MeasurementsTable measurementsTable = MeasurementsTable.of(measurements);
-    Collection<String> belowThresholdAssignments = new ArrayList();
-
-    for (String component : measurementsTable.uniqueComponents()) {
-      MeasurementsTable componentData = measurementsTable.component(component).type(metricName);
-      for (String instance : componentData.uniqueInstances()) {
-        if (componentData.instance(instance).get(0).value() < lowThreshold) {
-          LOG.fine(String.format("Instance %s has value %s below the limit", instance,
-                                 componentData.instance(instance).get(0).value()));
-          belowThresholdAssignments.add(instance);
+    if (measurements.size() > 0) {
+      MeasurementsTable measurementsTable = context.measurements().type(metricName).sort(false, SortKey.TIME_STAMP);
+      Collection<String> belowThresholdAssignments = new ArrayList();
+      for (String component : measurementsTable.uniqueComponents()) {
+        MeasurementsTable componentData = measurementsTable.component(component);
+        for (String instance : componentData.uniqueInstances()) {
+          MeasurementsTable instanceData = measurementsTable.instance(instance).last((int) noCheckpoints);
+          if (instanceData.valueBetween(Double.MIN_VALUE, lowThreshold).size() == noCheckpoints) {
+            LOG.fine(String.format("Instance %s has values below the limit for the last %s checkpoints", instance,
+                                   noCheckpoints));
+            belowThresholdAssignments.add(instance);
+          }
         }
       }
-    }
-    if (belowThresholdAssignments.size() > 0) {
-      Symptom s = new Symptom(Utils.getCompositeName(SYMPTOM_LOW, metricName), context.checkpoint(),
-                              belowThresholdAssignments);
-      result.add(s);
+      if (belowThresholdAssignments.size() > 0) {
+        Symptom s = new Symptom(Utils.getCompositeName(SYMPTOM_LOW, metricName), context.checkpoint(),
+                                belowThresholdAssignments);
+        result.add(s);
+      }
     }
     return result;
   }
