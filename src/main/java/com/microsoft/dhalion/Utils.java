75c7fcde2d0aa35866e3816d579508fbf788151c
avflor <avflor@microsoft.com>
2018-06-18 11:28:27 -0700

    Updated Utils class

--- src/main/java/com/microsoft/dhalion/Utils.java
+++ src/main/java/com/microsoft/dhalion/Utils.java
@@ -1,8 +1,20 @@
 package com.microsoft.dhalion;
 
+import java.util.ArrayList;
+
+import static java.awt.SystemColor.text;
+
 public class Utils {
 
-  public static String getCompositeName(String text, String metricName) {
-    return text + "_" + metricName;
+  public static String getCompositeName(String... names) {
+    if (names.length > 0) {
+      String text = names[0];
+      for (int i = 1; i < names.length; i++) {
+        text = text + "_" + names[i];
+      }
+      return text;
+    }
+    return "";
   }
+
 }
