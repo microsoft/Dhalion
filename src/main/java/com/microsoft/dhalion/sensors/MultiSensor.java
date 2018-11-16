4836340ae1e7a6269d01799a5064a411af6b05cc
Abhishek Modi <abmod@microsoft.com>
2018-11-16 20:13:09 +0530

    Adding MultiSensor which takes as input a collection of metric names and collects the values for those

---
+++ src/main/java/com/microsoft/dhalion/sensors/MultiSensor.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ *
+ * This program is made available under the terms of the MIT License.
+ * See the LICENSE file in the project root for more information.
+ */
+
+package com.microsoft.dhalion.sensors;
+
+import com.microsoft.dhalion.api.ISensor;
+import com.microsoft.dhalion.api.MetricsProvider;
+import com.microsoft.dhalion.conf.Config;
+import com.microsoft.dhalion.conf.Key;
+import com.microsoft.dhalion.core.Measurement;
+import com.microsoft.dhalion.policy.PoliciesExecutor.ExecutionContext;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The Multi Sensor consumes metrics from the metrics provider and returns them directly in their current form.
+ * It can be used when the metrics returned by the metrics provider do not require any further manipulation before
+ * being consumed by the detectors. Every time the Multi Sensor is invoked it returns metrics that correspond to the
+ * time window between the timestamp of the previous policy invocation and the current timestamp.
+ * <p>
+ * The Multi Sensor takes as input a collection of metric names, the metrics provider, and a set of components.
+ * It collects the values of the given metrics for these components.
+ */
+public class MultiSensor implements ISensor {
+  private static final Logger LOG = Logger.getLogger(MultiSensor.class.getName());
+  private static final Duration DEFAULT_METRIC_DURATION = Duration.ofSeconds(60);
+
+  private final Collection<String> metricNames;
+  private final Collection<String> components;
+
+  protected ExecutionContext context;
+  protected final MetricsProvider metricsProvider;
+
+  public MultiSensor(Config sysconfig, Collection<String> metricNames, MetricsProvider metricsProvider) {
+    this(sysconfig, metricNames, metricsProvider, null);
+  }
+
+  public MultiSensor(Config policyConf,
+      Collection<String> metricNames,
+      MetricsProvider metricsProvider,
+      Collection<String> components) {
+    this.metricNames = metricNames;
+    this.metricsProvider = metricsProvider;
+
+    if (components != null) {
+      this.components = components;
+    } else {
+      Object result = policyConf.get(Key.CONF_COMPONENT_NAMES.value());
+      if (result != null) {
+        this.components = Arrays.asList(result.toString().split(","));
+      } else {
+        this.components = Collections.emptyList();
+      }
+    }
+  }
+
+  @Override
+  public void initialize(ExecutionContext context) {
+    this.context = context;
+  }
+
+  @Override
+  public Collection<String> getMetricTypes() {
+    return metricNames;
+  }
+
+  /**
+   * Fetches the required metrics
+   *
+   * @return a Collection of Measurements
+   */
+  @Override
+  public Collection<Measurement> fetch() {
+    Instant startTime = context.checkpoint();
+    Duration duration = getDuration();
+    Collection<String> metrics = getMetricTypes();
+    LOG.fine(String.format("Trying to fetch %s @ %s for %s duration", getMetricTypes(), startTime, duration));
+
+    Collection<Measurement> measurements
+        = metricsProvider.getMeasurements(startTime, duration, metrics, getComponents());
+    if (LOG.isLoggable(Level.FINEST)) {
+      measurements.stream().map(Object::toString).forEach(LOG::finest);
+    }
+    return measurements;
+  }
+
+  protected Collection<String> getComponents() {
+    return components;
+  }
+
+  /**
+   * Returns the duration for which the metrics need to be collected
+   *
+   * @return duration in seconds
+   */
+  protected synchronized Duration getDuration() {
+    if (context.previousCheckpoint() == null || context.previousCheckpoint().equals(Instant.MIN)) {
+      return DEFAULT_METRIC_DURATION;
+    } else {
+      return Duration.between(context.previousCheckpoint(), context.checkpoint());
+    }
+  }
+}
